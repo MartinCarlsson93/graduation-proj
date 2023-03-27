ad246fb57a8766632f12aaf3f3a4ae125895d109
martincarlsson93 <martingunnarcarlsson@gmail.com>
2023-03-27 15:14:33 +0200

    Added Rubik as font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,16 @@ import "@/styles/globals.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
+import { Rubik } from "next/font/google";
+
+const rubik = Rubik({
+  weight: ["400", "600"],
+  style: ["normal", "italic"],
+});
+
 export default function App({ Component, pageProps }) {
   return (
-    <main>
+    <main className={rubik.className}>
       <Header />
       <Component {...pageProps} />
       <Footer />
