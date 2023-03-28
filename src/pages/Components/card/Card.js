6f2f2ad496a6103a974d99172c221cbee3c635db
martincarlsson93 <martingunnarcarlsson@gmail.com>
2023-03-28 16:52:20 +0200

    Added a couple of icons

--- src/pages/Components/card/Card.js
+++ src/pages/Components/card/Card.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import Image from "next/image";
 import styles from "./Card.module.css";
+import plus from "../../../../public/Add.svg";
+import minus from "../../../../public/Remove.svg";
+import favourites from "../../../../public/Favourites.svg";
+import campain from "../../../../public/Campain.svg";
 
 const Card = ({ name, description, image }) => {
   const [quantity, setQuantity] = useState(0);
+  const [isFavourite, setIsFavourite] = useState([]);
+  const [addCart, setAddCart] = useState([]);
 
   const addToCart = () => {
-    console.log(`Adding ${name} to cart`);
+    console.log(`Adding ${quantity} of ${name} to cart`);
   };
 
   const incrementQuantity = () => {
@@ -20,6 +27,10 @@ const Card = ({ name, description, image }) => {
 
   return (
     <div className={styles.card}>
+      <div className={styles.icons}>
+        <Image src={favourites} alt="favourites" width={30} height={30} />
+        <Image src={campain} alt="campain" width={30} height={30} />
+      </div>
       <div className={styles.imageContainer}>
         <img src={image} alt={name} className={styles.image} />
       </div>
@@ -27,14 +38,21 @@ const Card = ({ name, description, image }) => {
         <h3>{name}</h3>
         <p>{description}</p>
         <div className={styles.quantity}>
-          <button className={styles.quantityButton} onClick={incrementQuantity}>
-            +
-          </button>
-
+          <Image
+            src={minus}
+            alt="minus"
+            width={30}
+            height={30}
+            onClick={decrementQuantity}
+          />
           <span className={styles.quantityValue}>{quantity}</span>
-          <button className={styles.quantityButton} onClick={decrementQuantity}>
-            -
-          </button>
+          <Image
+            src={plus}
+            alt="plus"
+            width={30}
+            height={30}
+            onClick={incrementQuantity}
+          />
         </div>
         <button className={styles.addToCartButton} onClick={addToCart}>
           Add to cart
