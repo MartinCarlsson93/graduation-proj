a9b6560994bd930efcfa66fe2d88850f0e44f497
josefineweine <114985858+josefineweine@users.noreply.github.com>
2023-03-28 21:43:19 +0200

    footer responsive and changed layout

--- src/pages/Components/Header.js
+++ src/pages/Components/Header.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import weblogo from "../../../public/Web-logo.png";
 import Image from "next/image";
-import ShoppingCart from "../../../public/ShoppingCart.svg"
+import ShoppingCart from "../../../public/ShoppingCart.svg";
+
 
 function Header() {
   const router = useRouter();
@@ -13,42 +13,39 @@ function Header() {
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
   };
+
   return (
     <div className="header">
-      <Image src={weblogo} alt="Grocify-logo" width={240} height={180} />
+      <a href="/">
+        <Image src="/Web-logo.png" alt="Grocify-logo" width={240} height={180} />
+      </a>
       <nav>
         <ul className={menuVisible ? "nav-links visible" : "nav-links"}>
-        <Image src={ShoppingCart} alt="Shopping-cart" width={50} height={50} />
-          <li className={isActive("/") ? "active" : ""}>
-            <button onClick={() => router.push("/")}>Home</button>
-          </li>
-          <li className={isActive("/contact") ? "active" : ""}>
-            <button onClick={() => router.push("/contact")}>Contact us</button>
+          <li>
+          <Image src="/userCircle.svg" alt="userCircle" width={30} height={30} />
+            <Image src={ShoppingCart} alt="Shopping-cart" width={30} style={{marginLeft: "10px"}} /> 
           </li>
           <li className={isActive("/campaigns") ? "active" : ""}>
             <button onClick={() => router.push("/campaigns")}>Campaigns</button>
           </li>
+          <li className={isActive("/contact") ? "active" : ""}>
+            <button onClick={() => router.push("/contact")}>Contact Us</button>
+          </li>
           <li className="dropdown">
-        <button className="dropbtn">
-          More
-          <div className="arrow-down"></div>
-        </button>
-        <div className="dropdown-content">
-          <a href="#">Subpage 1</a>
-          <a href="#">Subpage 2</a>
-          <a href="#">Subpage 3</a>
-        </div>
-      </li>
-
+            <button className="dropbtn">
+              More
+              <div className="arrow-down"></div>
+            </button>
+            <div className="dropdown-content">
+              <a href="#">Subpage 1</a>
+              <a href="#">Subpage 2</a>
+              <a href="#">Subpage 3</a>
+            </div>
+          </li>
         </ul>
-  
-       
-
       </nav>
     </div>
   );
 }
 
 export default Header;
-
-
