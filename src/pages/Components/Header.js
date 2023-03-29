37a545675169f2d9f347d3e1c55023590f56fd6a
martincarlsson93 <martingunnarcarlsson@gmail.com>
2023-03-29 09:37:30 +0200

    changes

--- src/pages/Components/Header.js
+++ src/pages/Components/Header.js
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import ShoppingCart from "../../../public/ShoppingCart.svg";
 
-
 function Header() {
   const router = useRouter();
   const [menuVisible, setMenuVisible] = useState(false);
@@ -17,36 +16,55 @@ function Header() {
   return (
     <div className="header">
       <a href="/">
-        <Image src="/Web-logo.png" alt="Grocify-logo" width={240} height={180} />
+        <Image
+          src="/Web-logo.png"
+          alt="Grocify-logo"
+          width={240}
+          height={100}
+        />
       </a>
+      {/* <nav>
+        <ul className={menuVisible ? "nav-links visible" : "nav-links"}>
+          <li>
+            <Image
+              src="/userCircle.svg"
+              alt="userCircle"
+              width={30}
+              height={30}
+            />
+            <Image
+              src={ShoppingCart}
+              alt="Shopping-cart"
+              width={30}
+              style={{ marginLeft: "10px" }}
+            />
+          </li>
+        </ul>
+      </nav> */}
       <nav>
         <ul className={menuVisible ? "nav-links visible" : "nav-links"}>
           <li>
-          <Image src="/userCircle.svg" alt="userCircle" width={30} height={30} />
-            <Image src={ShoppingCart} alt="Shopping-cart" width={30} style={{marginLeft: "10px"}} />
-            </li> 
-            </ul>
-      <Image src={weblogo} alt="Grocify-logo" width={240} height={100} />
-</nav>
-      <nav>
-        <ul className={menuVisible ? "nav-links visible" : "nav-links"}>
-          <Image
-            src={ShoppingCart}
-            alt="Shopping-cart"
-            width={50}
-            height={50}
-          />
-          <li className={isActive("/") ? "active" : ""}>
-            <button onClick={() => router.push("/")}>Home</button>
+            <Image
+              src="/userCircle.svg"
+              alt="userCircle"
+              width={30}
+              height={30}
+              style={{ cursor: "pointer" }}
+              onClick={() => router.push("/login")}
+            />
+            <Image
+              src={ShoppingCart}
+              alt="Shopping-cart"
+              width={30}
+              style={{ marginLeft: "10px", cursor: "pointer" }}
+              onClick={() => router.push("/cart")}
+            />
           </li>
           <li className={isActive("/contact") ? "active" : ""}>
             <button onClick={() => router.push("/contact")}>Contact us</button>
           </li>
           <li className={isActive("/campaigns") ? "active" : ""}>
             <button onClick={() => router.push("/campaigns")}>Campaigns</button>
-          </li>
-          <li className={isActive("/contact") ? "active" : ""}>
-            <button onClick={() => router.push("/contact")}>Contact Us</button>
           </li>
           <li className="dropdown">
             <button className="dropbtn">
@@ -63,7 +81,6 @@ function Header() {
       </nav>
     </div>
   );
-  
 }
 
 export default Header;
