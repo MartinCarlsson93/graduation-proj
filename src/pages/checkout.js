c6498213ca49a05edf07a4426ebae555700ca190
martincarlsson93 <martingunnarcarlsson@gmail.com>
2023-03-30 15:19:05 +0200

    Small changes

---
+++ src/pages/checkout.js
@@ -0,0 +1,138 @@
+import { useState } from "react";
+
+export default function Checkout() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsSubmitting(true);
+
+    // Send checkout data to server
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        address,
+        city,
+        country,
+        zipCode,
+        cardNumber,
+        expiryDate,
+        cvv,
+      }),
+    });
+
+    setIsSubmitting(false);
+
+    if (response.ok) {
+      // Redirect to thank you page
+      window.location.href = "/thank-you";
+    } else {
+      // Handle error
+      console.error("Checkout failed.");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="address">Address:</label>
+        <input
+          type="text"
+          id="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="city">City:</label>
+        <input
+          type="text"
+          id="city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="country">Country:</label>
+        <input
+          type="text"
+          id="country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="zipCode">Zip Code:</label>
+        <input
+          type="text"
+          id="zipCode"
+          value={zipCode}
+          onChange={(e) => setZipCode(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="cardNumber">Card Number:</label>
+        <input
+          type="text"
+          id="cardNumber"
+          value={cardNumber}
+          onChange={(e) => setCardNumber(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="expiryDate">Expiry Date:</label>
+        <input
+          type="text"
+          id="expiryDate"
+          value={expiryDate}
+          onChange={(e) => setExpiryDate(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="cvv">CVV:</label>
+        <input
+          type="text"
+          id="cvv"
+          value={cvv}
+          onChange={(e) => setCvv(e.target.value)}
+        />
+      </div>
+      <button type="submit" disabled={isSubmitting}>
+        {isSubmitting ? "Submitting..." : "Submit"}
+      </button>
+    </form>
+  );
+}
