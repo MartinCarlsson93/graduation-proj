0ab3528b0f3e59d9bad553d3d01d9914ab497cf9
Adrian P. Mate <adrian.p.mate@gmail.com>
2023-03-31 15:10:31 +0200

    Added some scuffed styling to cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,14 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "./Components/hero/Hero";
+
+// You can replace this with data fetched from your API
+const sampleData = [
+  { id: 1, name: "Item 1", price: 10 },
+  { id: 2, name: "Item 2", price: 20 },
+  { id: 3, name: "Item 3", price: 30 },
+];
+
 function Cart() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItem = (item) => {
+    const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
+    if (existingItem) {
+      updateItemQuantity(item.id, existingItem.quantity + 1);
+    } else {
+      setCartItems([...cartItems, { ...item, quantity: 1 }]);
+    }
+  };
+
+  const updateItemQuantity = (id, quantity) => {
+    if (quantity <= 0) {
+      removeItem(id);
+    } else {
+      const updatedCartItems = cartItems.map((item) =>
+        item.id === id ? { ...item, quantity } : item
+      );
+      setCartItems(updatedCartItems);
+    }
+  };
+
+  const removeItem = (id) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedCartItems);
+  };
+
   return (
     <div className="main-container">
       <Hero header="Cart" />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
-        pulvinar sapien, quis varius lacus euismod et. Etiam
-        idasdasdasdasdasdasdas
-      </p>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem
+            key={item.id}
+            item={item}
+            updateItemQuantity={updateItemQuantity}
+            removeItem={removeItem}
+          />
+        ))}
+      </div>
+      <button onClick={() => addItem(sampleData[0])}>Add Item 1</button>
+      <button onClick={() => addItem(sampleData[1])}>Add Item 2</button>
+      <button onClick={() => addItem(sampleData[2])}>Add Item 3</button>
+      <button onClick={() => console.log("Implement Stripe checkout")}>
+        Checkout
+      </button>
+    </div>
+  );
+}
+
+function CartItem({ item, updateItemQuantity, removeItem }) {
+  return (
+    <div className="cart-item">
+      <h3>{item.name}</h3>
+      <p>Price: ${item.price}</p>
+      <button onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>
+        -
+      </button>
+      <span>{item.quantity}</span>
+      <button onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>
+        +
+      </button>
+      <button onClick={() => removeItem(item.id)}>Delete</button>
     </div>
   );
 }
