6e2ed904ef4630f50307a15d787a74c5bc069a9a
martincarlsson93 <martingunnarcarlsson@gmail.com>
2023-04-03 12:10:37 +0200

    Login state

--- src/pages/my-profile.js
+++ src/pages/my-profile.js
@@ -3,19 +3,26 @@ import { useRouter } from "next/router";
 import Hero from "./Components/hero/Hero";
 import formstyles from "../styles/form.module.css";
 import { useState, useEffect, useCallback } from "react";
-function MyProfile() {
-  const [loggedIn, setLoggedIn] = useState(false);
+function MyProfile({ loggedIn, logIn }) {
   const router = useRouter();
+
+  console.log(loggedIn);
+
+  let users = require("../../public/Data/person.json");
+
+  useEffect(() => {
+    if (loggedIn === false) {
+      router.push("/login");
+    }
+  }),
+    [loggedIn];
+
   return (
     // TODO SET A HANDLER THAT COMES HERE IF LOGGED IN, OTHERWISE GO TO LOGIN PAGE
     <div className="main-container">
       <Hero header="My profile" />
-      <button
-        className={formstyles.contentbutton}
-        onClick={() => router.push("/login")}
-      >
-        Login
-      </button>
+      <p>rederecting...</p>
+      <p>Du är inloggad nu</p>
     </div>
   );
 }
