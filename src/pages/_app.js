afe057d67a3e1c65c43a856c16ec58f9825f0624
martincarlsson93 <martingunnarcarlsson@gmail.com>
2023-04-03 14:33:02 +0200

    Changed campains to favourites and used Favourite context to handle state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 import "@/styles/globals.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
+import { FavouriteProvider } from "../pages/context/favouriteContext";
 
 // import { Rubik } from "next/font/google";
 
@@ -12,11 +13,16 @@ import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [addToCart, setAddToCart] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+
   const logIn = () => setLoggedIn(true);
   return (
     <main>
       <Header />
-      <Component {...pageProps} loggedIn={loggedIn} logIn={logIn} />
+      <FavouriteProvider>
+        <Component {...pageProps} loggedIn={loggedIn} logIn={logIn} />
+      </FavouriteProvider>
       <Footer />
     </main>
   );
