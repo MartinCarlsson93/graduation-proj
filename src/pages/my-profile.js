dd4bd29e2d4a2941c0b3b443053c89fe96521dd9
josefineweine <114985858+josefineweine@users.noreply.github.com>
2023-04-03 17:36:26 +0200

    myprofile

--- src/pages/my-profile.js
+++ src/pages/my-profile.js
@@ -1,21 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import Hero from "./Components/hero/Hero";
 import formstyles from "../styles/form.module.css";
-import { useState, useEffect, useCallback } from "react";
+
 function MyProfile() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [person, setPerson] = useState(null);
   const router = useRouter();
-  return (
-    // TODO SET A HANDLER THAT COMES HERE IF LOGGED IN, OTHERWISE GO TO LOGIN PAGE
-    <div className="main-container">
-      <Hero header="My profile" />
+
+  useEffect(() => {
+    fetch("../data/person.json")
+      .then((response) => response.json())
+      .then((data) => setPerson(data[0]));
+  }, []);
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
+  const renderProfile = (person) => {
+    return (
+      <div className={formstyles.profile}>
+        <div className={formstyles.profileHeader}>
+          <h2 className={formstyles.profileHeading}>{`${person.firstname} ${person.lastname}`}</h2>
+          <button className={formstyles.logoutButton} onClick={handleLogout}>
+            Logga ut
+          </button>
+        </div>
+        <div className={formstyles.profileContent}>
+          <div className={formstyles.roundProfileImage}>
+            <Image
+              src="/Assets/images/0.jpg"
+              alt="Profile Image"
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className={formstyles.profileInfo}>
+            <p>
+              <strong>Användarnamn: </strong>
+              {person.username}
+            </p>
+            <p>
+              <strong>Email: </strong>
+              {person.email}
+            </p>
+            <p>
+              <strong>Födelsedag: </strong>
+              {person.birthday}
+            </p>
+            <p>
+              <strong>Förnamn: </strong>
+              {person.firstname}
+            </p>
+            <p>
+              <strong>Efternamn: </strong>
+              {person.lastname}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderLoginButton = () => {
+    return (
       <button
         className={formstyles.contentbutton}
-        onClick={() => router.push("/login")}
+        onClick={() => setLoggedIn(true)}
       >
-        Login
+        Logga in
       </button>
+    );
+  };
+
+  return (
+    <div className={formstyles.container}>
+      <Hero header="Min profil" />
+      {loggedIn ? (person && renderProfile(person)) : renderLoginButton()}
     </div>
   );
 }
