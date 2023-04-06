611c34202bf84c8cb92cac48b2376df62c050a51
martincarlsson93 <martingunnarcarlsson@gmail.com>
2023-04-06 15:09:25 +0200

    Successful

--- src/pages/succesful.js
+++ src/pages/succesful.js
@@ -1,5 +1,24 @@
+import rocketLaunch from "../../public/Assets/svgs/RocketLaunch.svg";
+import Image from "next/image";
+
 function Succesful() {
-  return <div>Tack</div>;
+  return (
+    <div className="main-container">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Image src={rocketLaunch} alt="Rocket" width={200} height={300} />
+
+        <h2>Your order has Succesfully been registered</h2>
+        <h4>Thank you for ordering from Grocify</h4>
+        <h4>Your ordernumber is: 2462 2315</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Succesful;
