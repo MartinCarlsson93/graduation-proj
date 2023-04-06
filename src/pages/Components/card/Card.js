4e23255a9c9ac41096f540cc57577f99318a8bd8
martincarlsson93 <martingunnarcarlsson@gmail.com>
2023-04-06 09:47:43 +0200

    Fixes for merge

--- src/pages/Components/card/Card.js
+++ src/pages/Components/card/Card.js
@@ -1,51 +1,7 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-const Card = ({
-  name,
-  description,
-  image,
-  price,
-  onAddToCart,
-  onRemoveFromCart,
-}) => {
-  const [quantity, setQuantity] = useState(1);
-  const { items, addToFavourites } = useContext(FavouriteContext);
-
-  const addToCart = () => {
-    if (onAddToCart) {
-      onAddToCart({
-        name,
-        description,
-        image,
-        price,
-        id: name,
-        quantity: parseInt(quantity, 10),
-      });
-    }
-  };
-  const incrementQuantity = () => {
-    setQuantity((prevQuantity) => prevQuantity + 1);
-  };
-
-  const decrementQuantity = () => {
-    if (quantity > 0) {
-      setQuantity((prevQuantity) => prevQuantity - 1);
-    }
-  };
-
-  const addFavourite = () => {
-    const item = {
-      name: name,
-      description: description,
-      image: image,
-      price: price,
-    };
-    if (!items.includes(item.name)) {
-      addToFavourites(item);
-    }
-  };
-
+const Card = ({ name, description, image, price }) => {
   return (
     <div className={styles.card}>
       <div className={styles.imageContainer}>
@@ -55,34 +11,6 @@ const Card = ({
         <h4>{name}</h4>
         <p style={{ height: "80px" }}>{description}</p>
         <p>Price: ${price}</p>
-        <div className={styles.quantity}>
-          <Image
-            src={minus}
-            alt="minus"
-            width={30}
-            height={30}
-            onClick={decrementQuantity}
-          />
-          <span className={styles.quantityValue}>{quantity}</span>
-          <Image
-            src={plus}
-            alt="plus"
-            width={30}
-            height={30}
-            onClick={incrementQuantity}
-          />
-        </div>
-        <button className={styles.addToCartButton} onClick={addToCart}>
-          Add to cart
-        </button>
-        <button
-          className={styles.removeFromCartButton}
-          onClick={() => {
-            onRemoveFromCart({ id: name });
-          }}
-        >
-          Remove from cart
-        </button>
       </div>
     </div>
   );
